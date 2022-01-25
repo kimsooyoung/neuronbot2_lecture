8383ded6804a4a79b60b5c6d150d8534a03c5eb1
kimsooyoung <tge1375@naver.com>
2022-01-25 13:11:09 +0900

    Ongoing : fusionbot gazebo

---
+++ fusionbot_description/launch/fusionbot_gazebo.launch.py
@@ -0,0 +1,86 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch.actions import SetEnvironmentVariable
+from launch.substitutions import EnvironmentVariable
+
+from launch_ros.actions import Node
+
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+
+def generate_launch_description():
+
+    pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+
+    # Start Gazebo server
+    start_gazebo_server_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+        # launch_arguments={'world': world_path}.items()
+    )
+
+    # Start Gazebo client    
+    start_gazebo_client_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        node_executable='joint_state_publisher',
+        node_name='joint_state_publisher'
+    )
+
+    # Prepare Robot State Publisher Params
+    description_pkg_path = os.path.join(get_package_share_directory('fusionbot_description'))
+
+    # Robot State Publisher
+    urdf_file = os.path.join(description_pkg_path, 'urdf', 'fusionbot.urdf')
+    doc = xacro.parse(open(urdf_file))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+    param = {'use_sim_time': True, 'robot_description': doc.toxml()}
+
+    # xacro_file = os.path.join(description_pkg_path, 'urdf', 'fusionbot.urdf.xacro')
+    # xacro_file_config = xacro.process_file(xacro_file)
+    # robot_desc = xacro_file_config.toxml()
+    # param = {'robot_description': robot_desc}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        node_executable='robot_state_publisher',
+        node_name='robot_state_publisher',
+        output='screen',
+        # parameters=[urdf_file]
+        arguments=[urdf_file]
+    )
+
+    # Spawn Robot
+    spawn_entity = Node(
+        package='gazebo_ros', 
+        node_executable='spawn_entity.py',
+        output='screen',
+        arguments=['-topic', 'robot_description', '-entity', 'fusionbot'],
+    )
+
+    return LaunchDescription([
+        # ExecuteProcess(
+        #     cmd=['gzserver', '--verbose' , '-s', 'libgazebo_ros_init.so'],
+        #     output='screen'
+        # ),
+        # ExecuteProcess(
+        #     cmd=['gzclient'],
+        #     output='screen'
+        # ),
+        start_gazebo_server_cmd,
+        start_gazebo_client_cmd,
+        joint_state_publisher,
+        robot_state_publisher,
+        # spawn_entity,
+    ])
--- log.md
+++ log.md
@@ -1,3 +1,49 @@
+# fusion bot
+
+gazebo spawn, urdf만으로 하기
+
+skidbot은 잘 뜨는데... fusionbot은 안뜬다.
+이 둘의 차이가 뭘까...
+        
+```
+<mesh filename="package://fusionbot_description/meshes/base_link.stl" scale="0.001 0.001 0.001"/>
+```
+
+이런 package 때문일까??
+
+
+그런데, ROS1은 xacro로도 가제보 잘 나온다.
+robot_state_publisher 방식을 바꿔보자.
+
+xacro 사용으로 변경
+
+https://www.theconstructsim.com/how-to-use-xacro-in-ros2-gazebo/
+
+```
+[ERROR] [robot_state_publisher-4]: process has died [pid 30450, exit code 255, cmd '/opt/ros/eloquent/lib/robot_state_publisher/robot_state_publisher --ros-args --params-file /tmp/launch_params_xxfiap40'].
+```
+
+robot_state_publish에서 문제가 생기네...
+
+eloquent에서는 이렇게 arguments에 파일 경로를 바로 넣어주면 됨.
+
+```
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        node_executable='robot_state_publisher',
+        node_name='robot_state_publisher',
+        output='screen',
+        # parameters=[urdf_file]
+        arguments=[urdf_file]
+    )
+```
+
+그런데... 이제 spawn이 안되는 문제가 생긴다...
+
+spawn을 코딩으로 해보자 ㅠㅠ
+
+
+
 
 ```
 [ekf_node-1] Warning: Invalid frame ID "imu_link" passed to canTransform argument source_frame - frame does not exist
