9344fdd7f7057e18ddcceffb2cfaef2d491c7b5a
kimsooyoung <tge1375@naver.com>
2022-01-31 14:55:47 +0900

    Update : useless codes deleted during course dev

--- fusionbot_description/launch/fusionbot_gazebo.launch.py
+++ fusionbot_description/launch/fusionbot_gazebo.launch.py
@@ -43,46 +43,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py'))
     )
 
-    joint_state_publisher = Node(
-        package='joint_state_publisher',
-        node_executable='joint_state_publisher',
-        node_name='joint_state_publisher'
-    )
-
-    # Robot State Publisher
-    # urdf_file = os.path.join(description_pkg_path, 'urdf', 'fusionbot.urdf')
-    urdf_file = os.path.join(description_pkg_path, 'urdf', 'skidbot.urdf')
-    doc = xacro.parse(open(urdf_file))
-    xacro.process_doc(doc)
-    robot_description = {'robot_description': doc.toxml()}
-    param = {'use_sim_time': True, 'robot_description': doc.toxml()}
-
-    # xacro_file = os.path.join(description_pkg_path, 'urdf', 'fusionbot.urdf.xacro')
-    # xacro_file_config = xacro.process_file(xacro_file)
-    # robot_desc = xacro_file_config.toxml()
-    # param = {'robot_description': robot_desc}
-
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        node_executable='robot_state_publisher',
-        node_name='robot_state_publisher',
-        output='screen',
-        # parameters=[urdf_file]
-        arguments=[urdf_file]
-    )
-
-    # Spawn Robot
-    spawn_entity = Node(
-        package='gazebo_ros', 
-        node_executable='spawn_entity.py',
-        output='screen',
-        arguments=['-topic', 'robot_description', '-entity', 'fusionbot'],
-    )
-
     return LaunchDescription([
         start_gazebo_server_cmd,
         start_gazebo_client_cmd,
-        # joint_state_publisher,
-        # robot_state_publisher,
-        # spawn_entity,
     ])
