bbf0c2340c5252de7dbbbaf5702c35d116bae1ec
kimsooyoung <tge1375@naver.com>
2022-02-13 19:27:33 +0900

    Feature : fusion odom launch & rviz

--- neuronbot2_sensor_fusion/launch/rosbag_odom_rviz.launch.py
+++ neuronbot2_sensor_fusion/launch/rosbag_odom_rviz.launch.py
@@ -2,6 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
@@ -18,17 +19,34 @@ import xacro
 
 def generate_launch_description():
 
-    rviz_config_file = os.path.join(get_package_share_directory('neuronbot2_sensor_fusion'), 'rviz', 'neuronbot2_laser_odom_compare.rviz')
+    laser_rviz_config_file = os.path.join(get_package_share_directory('neuronbot2_sensor_fusion'), 
+    'rviz', 'neuronbot2_laser_odom_compare.rviz')
+
+    fusion_rviz_config_file = os.path.join(get_package_share_directory('neuronbot2_sensor_fusion'), 
+    'rviz', 'neuronbot2_fusion_odom_compare.rviz')
+
+    use_ekf =  LaunchConfiguration('use_ekf', default='false')
 
     # Launch RViz
-    rviz = Node(
+    rviz_laser = Node(
+        condition=UnlessCondition(use_ekf),
         package='rviz2',
         node_executable='rviz2',
         node_name='rviz2',
         output='screen',
-        arguments=['-d', rviz_config_file]
+        arguments=['-d', laser_rviz_config_file]
+    )
+
+    rviz_fusion = Node(
+        condition=IfCondition(use_ekf),
+        package='rviz2',
+        node_executable='rviz2',
+        node_name='rviz2',
+        output='screen',
+        arguments=['-d', fusion_rviz_config_file]
     )
 
     return LaunchDescription([
-        rviz,
+        rviz_laser,
+        rviz_fusion
     ])
